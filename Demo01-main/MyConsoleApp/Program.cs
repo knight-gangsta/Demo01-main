6da7a65d97950f55a95e90b127348a1c78ce6e34
guasonsad <guasonsad@GUASON-KING>
2024-04-09 11:31:01 -0500

    Library Figgle

--- Demo01-main/MyConsoleApp/Program.cs
+++ Demo01-main/MyConsoleApp/Program.cs
@@ -7,8 +7,8 @@
 			MessageWriter MyWriter = new MessageWriter();
 			MyWriter.Write("HELLO, WRITER!");
 
-			FiggleWriter FWriter = new FiggleWriter();
-			FWriter.Write("Hello, Figgle!!!");
+			/*FiggleWriter FWriter = new FiggleWriter();
+			FWriter.Write("Hello, Figgle!!!");*/
 		}
 	}
 }
--- Demo01-main/MyLibrary/FiggleWriter.cs
+++ Demo01-main/MyLibrary/FiggleWriter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,4 +13,4 @@ namespace MyLibrary
             Console.WriteLine(Figgle.FiggleFonts.Standard.Render(message));
         }
     }
-}
+}*/
